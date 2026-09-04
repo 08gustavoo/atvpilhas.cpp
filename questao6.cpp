a0735211cd861b53ab178f85d8e653f113f742a8
Gustavo Ribeiro <gustavors29082@gmail.com>
2026-09-04 16:24:27 -0300

    questao6.cpp

--- questao6.cpp
+++ questao6.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
 
+bool combina(char abertura, char fechamento) {
+    return (abertura == '(' && fechamento == ')') ||
+           (abertura == '[' && fechamento == ']') ||
+           (abertura == '{' && fechamento == '}');
+}
+
+int main() {
+    string expressao;
+    stack<char> pilha;
+
+    cout << "Digite a expressao: ";
+    getline(cin, expressao);
+
+    bool valida = true;
+
+    for (char c : expressao) {
+        if (c == '(' || c == '[' || c == '{') {
+            pilha.push(c);
+        }
+        else if (c == ')' || c == ']' || c == '}') {
+            if (pilha.empty() || !combina(pilha.top(), c)) {
+                valida = false;
+                break;
+            }
+            pilha.pop();
+        }
+    }
+
+    if (!pilha.empty())
+        valida = false;
+
+    if (valida)
+        cout << "Expressao valida.\n";
+    else
+        cout << "Expressao invalida.\n";
+
+    return 0;
+}
