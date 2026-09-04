d030f9e36fa5d2692cfe3584ffe111e56cbf989c
Gustavo Ribeiro <gustavors29082@gmail.com>
2026-09-04 10:12:32 -0300

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> pilha;
+    int numero;
+
+    cout << "Digite 5 números inteiros:" << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> numero;
+        pilha.push(numero);
+    }
+
+    cout << "\nElemento no topo: " << pilha.top() << endl;
+
+    cout << "\nQuantidade de elementos: " << pilha.size() << endl;
+
+    pilha.pop();
+
+    cout << "\nNovo elemento no topo: " << pilha.top() << endl;
+
+    return 0;
+}
 
