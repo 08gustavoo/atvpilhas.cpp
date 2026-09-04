64ee8d0f97978aa41589816cd48f4ac88bdfc4af
Gustavo Ribeiro <gustavors29082@gmail.com>
2026-09-04 16:23:17 -0300

    questao3.cpp

--- questao3.cpp
+++ questao3.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
 
+int main() {
+    string texto;
+    stack<char> pilha;
+
+    cout << "Digite uma palavra ou frase: ";
+    getline(cin, texto);
+
+    for (char letra : texto)
+        pilha.push(letra);
+
+    cout << "Invertido: ";
+
+    while (!pilha.empty()) {
+        cout << pilha.top();
+        pilha.pop();
+    }
+
+    cout << endl;
+    return 0;
+}
